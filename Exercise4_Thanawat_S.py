c5caafb3023b968af5e16c6baa51810db4824d72
Ginlomm <95574200+Ginlomm@users.noreply.github.com>
2021-12-05 23:50:25 +0700

    Exercise4_Thanawat_S.py
    Ex4

---
+++ Exercise4_Thanawat_S.py
@@ -0,0 +1,10 @@
+fe = 75.5
+gb = 60
+ic = 65
+cp = 75
+
+print("-----Your Score-----")
+print("Foundation English", fe)
+print("General Business", gb)
+print("Introduction to Computer Systems", ic)
+print("Computer Programming", cp)
